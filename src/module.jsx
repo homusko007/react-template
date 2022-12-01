1e6a9f1eefbb048addbf488cbb4efff49ae8235b
Natalia Homusko <avgust_net85@mail.ru>
2022-12-01 12:39:08 +0300

    react tenplate

---
+++ src/module.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import phone from './img/image.jpg'
+import './index.scss';
+
+
+const Module = () => {
+    return (
+    <>
+        <h1>Hello React!</h1>
+        <img width={500} src={phone} />
+    </>
+    )
+};
+
+export default Module;
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+
+// нужно определить какой у нас mode(режим) 
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+    mode,
+    devtool: mode === 'development' ? 'eval' : false,
+    entry: path.resolve(__dirname, 'src', 'index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: '[name][contenthash].js',
+        assetModuleFilename: 'assets/[hash][ext][query]',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'], // возможность импортировать файлы и не указывать их расширение
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: true,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public', 'index.html')
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.[tj]sx?$/i,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-react', '@babel/preset-env'],
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: ['postcss-preset-env']
+                            }
+                        }
+                    },
+                    'sass-loader']
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(wof2|woff2|ttf)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[hash][ext]',
+                }
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+        ]
+
+    }
+}
